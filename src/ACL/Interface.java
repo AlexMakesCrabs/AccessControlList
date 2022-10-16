30d4c7dad4964201a4f21c0433f48b49cf1f4efe
AlexMakesCrabs <nathaniel.d.bare@gmail.com>
2022-10-16 00:27:40 -0400

    Update Interface.java
    Simple update

--- src/ACL/Interface.java
+++ src/ACL/Interface.java
@@ -5,14 +5,17 @@ public class Interface {
 	protected Room[] doors = {new Room("Room1"), new Room("Room2"), new Room("Room3"), new Room("Main")};
 	protected Employee[] emps = {new Employee("Andy"), new Employee("Bri"), new Employee("Josh")};
 	
-	
+	public Room newDoor(int doornum) {
+		if (doornum>=doors.length || doornum<0) return null;
+		return doors[doornum];
+	}
 	
 	
 	
 	public static void main(String[] args){
 		
 		Interface I = new Interface();
-		I.acl.push(new Entry(I.doors[0],I.emps[0]));
+		I.acl.push(new Entry(I.newDoor(0),I.emps[0]));
 		System.out.println(I.acl.peek());
 		
 	}
